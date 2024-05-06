bc350806f48eb26be0a77deed866742c84fb728c
shs9267 <shs9267@gmail.com>
2024-05-06 11:16:08 +0900

    rand, min
    랜덤, 최솟값 추가

--- newton_method.c
+++ newton_method.c
@@ -1,5 +1,7 @@
 #include <stdio.h>
 #include <math.h>
+#include <stdlib.h>
+#include <time.h>
 
 // 다항식 함수
 double p1(double coefficient[], int degree, double x) {
@@ -19,32 +21,46 @@ double p2(double coefficient[], int degree, double x) {
     return result;
 }
 
+// 다항식의 이계도함수
+double p3(double coefficient[], int degree, double x) {
+    double result = 0;
+    for (int i = 0; i < degree; i++) {
+        result += coefficient[i] * (degree - i) * (degree - i - 1) * pow(x, degree - i - 2);
+    }
+    return result;  
+}
+
 // Newton's Method 구현
 double newton(double coefficient[], int degree, double x0, double tolerance, int max_repeat) {
     double x = x0;
     int repeat = 1;
     
     // 반복문을 이용한 Newton's Method
-    while (p1(coefficient, degree, x) > tolerance && repeat < max_repeat) {
-        x = x - p1(coefficient, degree, x) / p2(coefficient, degree, x); // Newton's Method 공식
+    while (fabs(p2(coefficient, degree, x)) > tolerance && repeat < max_repeat) {
+        x = x - p2(coefficient, degree, x) / p3(coefficient, degree, x); // Newton's Method 공식
         repeat++;
     }
+
+    double result = p1(coefficient, degree, x);
 
     // 결과 출력
     if (repeat == max_repeat) {
         printf("최대 반복 횟수에 도달했습니다. 근을 찾을 수 없습니다.\n");
     } else {
         printf("반복한 횟수입니다: %d\n", repeat);
-        printf("근을 찾았습니다: %lf\n", x);
+        printf("f(x)의 최솟값입니다: %lf\n", result);
     }
-    
-    return x;
+
+    return result;
 }
 
 int main() {
     int degree;
     double coefficient[11]; // 다항식의 계수
-    
+    double tolerance;
+    int max_repeat;
+    int num_trials;
+
     // 사용자로부터 차수 입력 받기
     printf("다항식의 차수를 입력하세요 (최대 10) : ");
     scanf("%d", &degree);
@@ -56,20 +72,33 @@ int main() {
         scanf("%lf", &coefficient[i]);
     }
     
-    double first;
-    double tolerance;
-    int max_repeat;
-    
-    // 초기 추정값, 허용 오차, 최대 반복 횟수 입력 받기
-    printf("초기 추정값을 입력하세요: ");
-    scanf("%lf", &first);
+    // 허용 오차와 최대 반복 횟수 입력 받기
     printf("허용 오차를 입력하세요: ");
     scanf("%lf", &tolerance);
     printf("최대 반복 횟수를 입력하세요: ");
     scanf("%d", &max_repeat);
+
+    // 시도 횟수 입력 받기
+    printf("몇 번의 시도를 하시겠습니까? ");
+    scanf("%d", &num_trials);
     
-    // Newton's Method 호출
-    newton(coefficient, degree, first, tolerance, max_repeat);
-    
+    srand(time(NULL));
+
+    double min_result = INFINITY; // 최솟값을 저장할 변수, 초기값은 무한대로 설정
+
+    // Newton's Method를 여러 번 호출하여 랜덤한 초기 추정값 사용
+    for (int i = 0; i < num_trials; i++) {
+        double random = (double)rand() / RAND_MAX * 200.0 - 100.0; // -100에서 100 사이의 double 값
+        printf("시도 %d:\n", i + 1);
+        double result = newton(coefficient, degree, random, tolerance, max_repeat);
+        printf("난수 값입니다: %lf\n", random);
+        if (result < min_result) {
+            min_result = result; // 현재의 f(x) 값이 최솟값보다 작으면 최솟값을 업데이트
+        }
+        printf("\n");
+    }
+
+    printf("최소 f(x) 값: %lf\n", min_result);
+
     return 0;
 }
